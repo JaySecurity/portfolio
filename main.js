0d57e8fb5c00a579fce582ebff77c403a6375141
JaySecurity <jasonnicholls1981@gmail.com>
2021-03-19 16:13:02 -0400

    modify nav bav

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 let cards = document.querySelectorAll('.card-container');
+let header = document.querySelector('header');
+let prevScroll = window.screenY;
+
+document.addEventListener('scroll', () => {
+  setTimeout(() => {
+    if (window.scrollY > prevScroll) {
+      prevScroll = window.scrollY;
+      header.style.top = '-100%';
+    }
+    if (window.scrollY < prevScroll) {
+      prevScroll = window.scrollY;
+      header.style.top = 0;
+    }
+  }, 100);
+});
 
 cards.forEach((card) => {
   card.addEventListener('mouseenter', (e) => {
