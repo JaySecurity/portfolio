cb33f10e5c43f2ee1a1b03e5ca58000febd17337
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-25 10:50:42 -0400

    restructure files

--- old/main.js
+++
@@ -1,7 +0,0 @@
-let cards = document.querySelectorAll('.card-container');
-
-cards.forEach((card) => {
-  card.addEventListener('click', (e) => {
-    card.style.backgroundColor = 'white';
-  });
-});
